3b972a62e2d75f2894178ac1e21aeb3bb97031c0
laurencecwj <laurence@c12.ai>
2025-12-19 12:14:49 +0800

    .

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from minio import Minio
 from minio.credentials import IamAwsProvider
 import configparser
 import io
+import boto3
 
 config = configparser.ConfigParser()
 config.read('./config.ini')
@@ -12,6 +13,8 @@ s3_endpoint = config['aws'].get('s3_endpoint', 's3.cn-northwest-1.amazonaws.com.
 print("using s3 endpoint: ", s3_endpoint)
 object_name = "py.foo.bar"
 print("using s3 object: ", object_name)
+region = config['aws'].get('s3_region', 'cn-northwest-1')
+print("using s3 region: ", region)
 
 provider = IamAwsProvider()
 client = Minio(
@@ -19,6 +22,8 @@ client = Minio(
     credentials=provider,
     secure=True
 )
+
+client2 = boto3.resource('s3', region_name=region).Bucket(bucket_name)
 
 def put_object():
     try:
@@ -55,6 +60,42 @@ def rm_object():
     except Exception as e:
         print(f"Error deleting {bucket_name}/{object_name} object: {e}")
 
+def put_object_boto3():
+    try:
+        data = b"Hello, MinIO!"
+        data_stream = io.BytesIO(data)
+    
+        client2.put_object(
+            Body=data_stream, Bucket=bucket_name, Key=object_name
+        )
+        print(f"'{object_name}' uploaded successfully to '{bucket_name}'.")
+    except Exception as e:
+        print(f"put object to {bucket_name}/{object_name} error: {e}")
+
+
+def get_object_boto3():
+    try:
+        obj = client2.Object(bucket_name=bucket_name, key=object_name)
+        object_data = obj.get()["Body"].read()
+        print(f"Downloaded '{object_name}': {object_data.decode('utf-8')}")
+    except Exception as e:
+        print(f"Error getting {bucket_name}/{object_name} object error: {e}")
+    finally:
+        pass
+
+def rm_object_boto3():
+    try:
+        client2.Object(object_name).delete()
+        print(f"'{object_name}' deleted successfully from '{bucket_name}'.")
+    except Exception as e:
+        print(f"Error deleting {bucket_name}/{object_name} object: {e}")
+
+print("testing minio sdk")
 put_object()
 get_object()
 rm_object()
+
+print("testing boto3 sdk")
+put_object_boto3()
+get_object_boto3()
+rm_object_boto3()
